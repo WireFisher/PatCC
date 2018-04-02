d24176cd1742a73ae54584b49ec576bc9d67e9d1
WireFisher <20874936+WireFisher@users.noreply.github.com>
2018-04-02 16:40:29 +0800

    make plotting code more beautiful (shift, scale)

--- opencv_utils.cxx
+++ opencv_utils.cxx
@@ -57,16 +57,18 @@ void plot_projected_edge_into_file(const char *filename, double *head_coord[2], 
                                    double min_x = 0.0, double max_x = 0.0, double min_y = 0.0, double max_y = 0.0)
 {
     std::vector<Edge*> edges;
-    int x_shift = 150;
-    int y_shift = 150;
+    int x_shift = 1500;
+    int y_shift = 1500;
+    int scale = 500;
 
     cv::Mat mat = cv::Mat::zeros(300*10, 300*10, CV_8UC3); /* rows, columns*/
 
-    cv::line(mat, cv::Point((x_shift-150) * 10, (y_shift+0) * 10), cv::Point((x_shift+150) * 10, (y_shift+0) * 10), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    cv::line(mat, cv::Point((x_shift+0) * 10, (y_shift-150) * 10), cv::Point((x_shift+0) * 10, (y_shift+150) * 10), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    cv::line(mat, cv::Point(-150*scale+x_shift,    0*scale+y_shift), cv::Point(150*scale+x_shift,   0*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    cv::line(mat, cv::Point(   0*scale+x_shift, -150*scale+y_shift), cv::Point(  0*scale+x_shift, 150*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
 
     for(int i = 0; i < num_edges; i++)
-        cv::line(mat, cv::Point((head_coord[0][i]+x_shift) * 10, (head_coord[1][i]+y_shift) * 10), cv::Point((tail_coord[0][i]+x_shift) * 10, (tail_coord[1][i]+y_shift) * 10),
+        cv::line(mat, cv::Point(head_coord[0][i] * scale + x_shift, head_coord[1][i] * scale + y_shift),
+                      cv::Point(tail_coord[0][i] * scale + x_shift, tail_coord[1][i] * scale + y_shift),
                  cv::Scalar(255, 255, 255), 1, cv::LINE_8);
 
     std::vector<int> compression_params;
@@ -85,24 +87,34 @@ void plot_points_info_file(const char *filename, double *x, double *y, int num,
                            double min_x = 0.0, double max_x = 0.0, double min_y = 0.0, double max_y = 0.0)
 {
     std::vector<Edge*> edges;
+    /* // for lonlat
+    int x_shift = 200;
+    int y_shift = 900;
+    int scale = 10;
+    */
+    int x_shift = 1500;
+    int y_shift = 1500;
+    int scale = 500;
 
-    cv::Mat mat = cv::Mat::zeros(180*10, 400*10, CV_8UC3); /* rows, columns*/
+    //cv::Mat mat = cv::Mat::zeros(180*10, 400*10, CV_8UC3); /* rows, columns*/
+    cv::Mat mat = cv::Mat::zeros(300*10, 300*10, CV_8UC3); /* rows, columns*/
 
-    cv::line(mat, cv::Point((0.0+20.0) * 10, (-90.0+90.0) * 10), cv::Point((0.0+20.0) * 10, (90.0+90.0) * 10), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    cv::line(mat, cv::Point((0.0+20.0) * 10, (90.0+90.0) * 10), cv::Point((360.0+20.0) * 10, (90.0+90.0) * 10), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    cv::line(mat, cv::Point((360.0+20.0) * 10, (90.0+90.0) * 10), cv::Point((360.0+20.0) * 10, (-90.0+90.0) * 10), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    cv::line(mat, cv::Point((360.0+20.0) * 10, (-90.0+90.0) * 10), cv::Point((0.0+20.0) * 10, (-90.0+90.0) * 10), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    cv::line(mat, cv::Point(-150*scale+x_shift,    0*scale+y_shift), cv::Point(150*scale+x_shift,   0*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    cv::line(mat, cv::Point(   0*scale+x_shift, -150*scale+y_shift), cv::Point(  0*scale+x_shift, 150*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+
+    //cv::line(mat, cv::Point(  0.0*scale+x_shift, -90*scale+y_shift), cv::Point(  0.0*scale+x_shift,  90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    //cv::line(mat, cv::Point(  0.0*scale+x_shift,  90*scale+y_shift), cv::Point(360.0*scale+x_shift,  90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    //cv::line(mat, cv::Point(360.0*scale+x_shift,  90*scale+y_shift), cv::Point(360.0*scale+x_shift, -90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    //cv::line(mat, cv::Point(360.0*scale+x_shift, -90*scale+y_shift), cv::Point(  0.0*scale+x_shift, -90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
 
     for(int i = 0; i < num; i++)
-        cv::circle(mat, cv::Point((x[i]+20.0) * 10, (y[i]+90.0) * 10), 1, cv::Scalar(255, 255, 255), -1, 8);
-    //cv::line(mat, cv::Point((x1+20.0) * 10, (y1+90.0) * 10), cv::Point((x2+20.0) * 10, (y2+90.0) * 10), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
-    //cv::line(mat, cv::Point(215.4 * 10, 90.0 * 10), cv::Point(143.6 * 10, 90 * 10), cv::Scalar(0, 255, 0), 2, cv::LINE_8);
+        cv::circle(mat, cv::Point(x[i]*scale+x_shift, y[i]*scale+y_shift), 1, cv::Scalar(255, 255, 255), -1, 8);
 
     if(min_x != 0.0 || max_x != 0.0 || min_y != 0.0 || max_y != 0.0) {
-        cv::line(mat, cv::Point((min_x+20.0) * 10, (min_y+90.0) * 10), cv::Point((min_x+20.0) * 10, (max_y+90.0) * 10), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
-        cv::line(mat, cv::Point((min_x+20.0) * 10, (max_y+90.0) * 10), cv::Point((max_x+20.0) * 10, (max_y+90.0) * 10), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
-        cv::line(mat, cv::Point((max_x+20.0) * 10, (max_y+90.0) * 10), cv::Point((max_x+20.0) * 10, (min_y+90.0) * 10), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
-        cv::line(mat, cv::Point((max_x+20.0) * 10, (min_y+90.0) * 10), cv::Point((min_x+20.0) * 10, (min_y+90.0) * 10), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
+        cv::line(mat, cv::Point(min_x*scale+x_shift, min_y*scale+y_shift), cv::Point(min_x*scale+x_shift, max_y*scale+y_shift), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
+        cv::line(mat, cv::Point(min_x*scale+x_shift, max_y*scale+y_shift), cv::Point(max_x*scale+x_shift, max_y*scale+y_shift), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
+        cv::line(mat, cv::Point(max_x*scale+x_shift, max_y*scale+y_shift), cv::Point(max_x*scale+x_shift, min_y*scale+y_shift), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
+        cv::line(mat, cv::Point(max_x*scale+x_shift, min_y*scale+y_shift), cv::Point(min_x*scale+x_shift, min_y*scale+y_shift), cv::Scalar(0, 0, 255), 2, cv::LINE_8);
     }
 
     std::vector<int> compression_params;
