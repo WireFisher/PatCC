201a69e1bf77b7883f255e4d2fe1ef25b2811e50
WireFisher <20874936+WireFisher@users.noreply.github.com>
2018-05-14 22:30:07 +0800

    add option for ploting mode(latlon or projection)

--- opencv_utils.cxx
+++ opencv_utils.cxx
@@ -56,7 +56,7 @@ void plot_projected_edge_into_file(const char *filename, double *head_coord[2], 
     std::vector<Edge*> edges;
     int x_shift = 1500;
     int y_shift = 1500;
-    int scale = 500;
+    int scale = 10;
     
     cv::Mat mat;
     if(filemode == PDLN_PLOT_FILEMODE_NEW)
@@ -96,29 +96,32 @@ void plot_projected_edge_into_file(const char *filename, double *head_coord[2], 
 }
 
 
-void plot_points_info_file(const char *filename, double *x, double *y, int num,
+void plot_points_info_file(const char *filename, double *x, double *y, int num, int mode,
                            double min_x, double max_x, double min_y, double max_y)
 {
-    std::vector<Edge*> edges;
-    /* // for lonlat
-    int x_shift = 200;
-    int y_shift = 900;
-    int scale = 10;
-    */
-    int x_shift = 1500;
-    int y_shift = 1500;
-    int scale = 500;
+    int x_shift, y_shift, scale;
+    cv::Mat mat;
 
-    //cv::Mat mat = cv::Mat::zeros(180*10, 400*10, CV_8UC3); /* rows, columns*/
-    cv::Mat mat = cv::Mat::zeros(300*10, 300*10, CV_8UC3); /* rows, columns*/
+    if(mode == PDLN_PLOT_GLOBAL) {
+        x_shift = 200;
+        y_shift = 900;
+        scale = 10;
+        mat = cv::Mat::zeros(180*10, 400*10, CV_8UC3); /* rows, columns*/
 
-    cv::line(mat, cv::Point(-150*scale+x_shift,    0*scale+y_shift), cv::Point(150*scale+x_shift,   0*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    cv::line(mat, cv::Point(   0*scale+x_shift, -150*scale+y_shift), cv::Point(  0*scale+x_shift, 150*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+        cv::line(mat, cv::Point(  0.0*scale+x_shift, -90*scale+y_shift), cv::Point(  0.0*scale+x_shift,  90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+        cv::line(mat, cv::Point(  0.0*scale+x_shift,  90*scale+y_shift), cv::Point(360.0*scale+x_shift,  90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+        cv::line(mat, cv::Point(360.0*scale+x_shift,  90*scale+y_shift), cv::Point(360.0*scale+x_shift, -90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+        cv::line(mat, cv::Point(360.0*scale+x_shift, -90*scale+y_shift), cv::Point(  0.0*scale+x_shift, -90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    }
+    else if(mode == PDLN_PLOT_PROJECTION) {
+        x_shift = 1500;
+        y_shift = 1500;
+        scale = 500;
+        mat = cv::Mat::zeros(300*10, 300*10, CV_8UC3); /* rows, columns*/
 
-    //cv::line(mat, cv::Point(  0.0*scale+x_shift, -90*scale+y_shift), cv::Point(  0.0*scale+x_shift,  90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    //cv::line(mat, cv::Point(  0.0*scale+x_shift,  90*scale+y_shift), cv::Point(360.0*scale+x_shift,  90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    //cv::line(mat, cv::Point(360.0*scale+x_shift,  90*scale+y_shift), cv::Point(360.0*scale+x_shift, -90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
-    //cv::line(mat, cv::Point(360.0*scale+x_shift, -90*scale+y_shift), cv::Point(  0.0*scale+x_shift, -90*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+        cv::line(mat, cv::Point(-150*scale+x_shift,    0*scale+y_shift), cv::Point(150*scale+x_shift,   0*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+        cv::line(mat, cv::Point(   0*scale+x_shift, -150*scale+y_shift), cv::Point(  0*scale+x_shift, 150*scale+y_shift), cv::Scalar(0, 255, 255), 1, cv::LINE_8);
+    }
 
     for(int i = 0; i < num; i++)
         cv::circle(mat, cv::Point(x[i]*scale+x_shift, y[i]*scale+y_shift), 1, cv::Scalar(255, 255, 255), -1, 8);
--- opencv_utils.h
+++ opencv_utils.h
@@ -8,12 +8,14 @@
 #define PDLN_PLOT_FILEMODE_NEW     0
 #define PDLN_PLOT_FILEMODE_APPEND  1
 
+#define PDLN_PLOT_GLOBAL        0
+#define PDLN_PLOT_PROJECTION    1
+
 void plot_edge_into_file(const char *filename, double *head_coord[2], double *tail_coord[2], int num_edges,
                          double min_x = 0.0, double max_x = 0.0, double min_y = 0.0, double max_y = 0.0);
 void plot_projected_edge_into_file(const char *filename, double *head_coord[2], double *tail_coord[2], int num_edges, int, int);
 
-void plot_points_info_file(const char *filename, double *x, double *y, int num,
+void plot_points_info_file(const char *filename, double *x, double *y, int num, int mode = PDLN_PLOT_PROJECTION,
                            double min_x = 0.0, double max_x = 0.0, double min_y = 0.0, double max_y = 0.0);
-
 
 #endif
