82cac8d1930562b7032859427e17cd1f8f581aca
WireFisher <20874936+WireFisher@users.noreply.github.com>
2018-09-28 00:07:34 +0800

    chore: add CPU affinity for OpenMP

--- unittest/testmain.cxx
+++ unittest/testmain.cxx
@@ -5,8 +5,32 @@
 #include "../processing_unit_mgt.h"
 #include "../delaunay_grid_decomposition_mgt.h"
 
+#include <omp.h>
+#include <sched.h>
+#include <sys/syscall.h>
+
 Grid_info_manager *grid_info_mgr;
 Process_thread_manager *process_thread_mgr;
+
+void set_cpu_affinity()
+{
+    #pragma omp parallel
+    {
+        cpu_set_t mask;
+
+        int omp_rank = omp_get_thread_num();
+        CPU_ZERO(&mask);
+        CPU_SET(omp_rank, &mask);
+        
+        pid_t tid = syscall(__NR_gettid);
+        if (sched_setaffinity(tid, sizeof(cpu_set_t), &mask) == -1) {
+            perror("sched_setaffinity");
+            exit(EXIT_FAILURE);
+        }
+        printf("Thread %d on CPU %d\n", omp_get_thread_num(), sched_getcpu());
+    }
+}
+
 
 int main(int argc, char** argv) {
     int result = 0;
@@ -23,6 +47,8 @@ int main(int argc, char** argv) {
 #ifndef TIME_PERF
     setvbuf(stdout, 0, 2, 0);
 #endif
+
+    set_cpu_affinity();
     result = RUN_ALL_TESTS();
     MPI_Finalize();
 
