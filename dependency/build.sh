531bbfa0071e7d74b581f7643503dabe85532bd4
WireFisher <20874936+WireFisher@users.noreply.github.com>
2019-04-07 15:24:05 +0800

    chore: update google test build script

--- dependency/build.sh
+++ dependency/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+GXX=g++
 GTEST_DIR=$(pwd)/googletest/
 GMOCK_DIR=$(pwd)/googlemock/
 
 cd ${GTEST_DIR}
-g++ -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -pthread -c ${GTEST_DIR}/src/gtest-all.cc
+${GXX} -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -pthread -c ${GTEST_DIR}/src/gtest-all.cc
 ar -rv libgtest.a gtest-all.o
 
 cd ${GMOCK_DIR}
-g++ -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} -pthread -c ${GTEST_DIR}/src/gtest-all.cc
-g++ -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} -pthread -c ${GMOCK_DIR}/src/gmock-all.cc
+${GXX} -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} -pthread -c ${GTEST_DIR}/src/gtest-all.cc
+${GXX} -isystem ${GTEST_DIR}/include -I${GTEST_DIR} -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} -pthread -c ${GMOCK_DIR}/src/gmock-all.cc
 ar -rv libgmock.a gtest-all.o gmock-all.o
 
 cd ${GMOCK_DIR}/../
